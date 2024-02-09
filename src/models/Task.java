3f9f7c0d8aaace5e8e71f2b4519398c71c12c047
asami.kayashima <www.amini.net@gmail.com>
2024-02-09 21:23:10 +0900

    データベース関連のファイルを追加

---
+++ src/models/Task.java
@@ -0,0 +1,64 @@
+
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "creationDate", nullable = false)
+    private Timestamp creationDate;
+
+    @Column(name = "updateDate", nullable = false)
+    private Timestamp updateDate;
+
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Timestamp getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Timestamp updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+}
+
